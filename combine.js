c833a5e2a480dd305478cb8477d28bd8220fee1b
panaminer08 <adamsrey@outlook.com>
2018-08-24 11:48:26 -0400

    adding combine

---
+++ combine.js
@@ -0,0 +1,4 @@
+const choosePikachu = axios.get("https://fizal.me/pokeapi/api/25.json")
+const chooseMienshao = axios.get("https://fizal.me/pokeapi/api/620.json")
+const choosePsyduck = axios.get("https://fizal.me/pokeapi/api/54.json")
+
